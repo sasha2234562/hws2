db9426e06ef9390af4e3ffde635c85d97735a018
Sasha <skalitsenia@mail.ru>
2023-06-25 17:00:19 +0300

    commit three

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,17 +1,17 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
-import { UserType } from './HW3'
+import {UserType} from './HW3'
 
 type GreetingContainerPropsType = {
     users: Array<UserType> // need to fix any
-    addUserCallback: (name : string)=>void // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
-export const pureAddUser = (name: string, setError: (error: string)=>void, setName: (name:string)=>void,
-                            addUserCallback: (name: string)=> void) => {
-    if(!name.trim()){
-        setError('ошибка!')
-    }else{
+export const pureAddUser = (name: string, setError: (error: string) => void, setName: (name: string) => void,
+                            addUserCallback: (name: string) => void) => {
+    if (!name.trim()) {
+        setError('Ошибка! Введите имя!')
+    } else {
         addUserCallback(name)
         setName('')
     }
@@ -19,15 +19,14 @@ export const pureAddUser = (name: string, setError: (error: string)=>void, setNa
 
 }
 
-export const pureOnBlur = (name: string, setError: (error: string)=>void) => { // если имя пустое - показать ошибку
-    if(!name.trim()) {
-        setError('error')
+export const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
+    if (!name.trim()) {
+        setError('Ошибка! Введите имя')
     }
-
 }
 
-export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: ()=> void) => {
-    if (e.key === 'Enter'){
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => void) => {
+    if (e.key === 'Enter') {
         addUser()
     }
     // если нажата кнопка Enter - добавить
@@ -38,16 +37,16 @@ export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: ()=> vo
 
 // более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
     // деструктуризация пропсов
     const [name, setName] = useState<string>('') // need to fix any
     const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
-         error && setError('')
+        error && setError('')
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
@@ -58,14 +57,14 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     }
 
     const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-        if(e.key === 'Enter' && name) {
+        console.log('Dasha')
+        if (e.key === 'Enter' && name) {
             addUser()
-
         }
     }
 
     const totalUsers = users.length // need to fix
-    const lastUserName = totalUsers ? users[users.length-1].name : '' // need to fix
+    const lastUserName = totalUsers ? users[users.length - 1].name : '' // need to fix
 
     return (
         <Greeting
